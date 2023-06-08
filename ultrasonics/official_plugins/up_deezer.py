8b3cf6ff2a2a8ada8b72d7d46b39240a53a75b75
DW1985 <dwortman@gmail.com>
2023-06-08 08:49:14 +0200

    Update up_deezer.py

--- ultrasonics/official_plugins/up_deezer.py
+++ ultrasonics/official_plugins/up_deezer.py
@@ -457,9 +457,7 @@ def run(settings_dict, **kwargs):
                 existing_ids = [str(item["id"]["deezer"])
                                 for item in existing_tracks]
                 s_existing_ids = str(existing_ids)
-                s_existing_tracks = str(existing_tracks)
-                print("existing ids:" + s_existing_ids)
-                print("existing tracks:" + s_existing_tracks)
+                log.debug("existing ids:" + s_existing_ids)
 
             # Add songs which don't already exist in the playlist
             new_ids = []
@@ -508,9 +506,11 @@ def run(settings_dict, **kwargs):
                     dz.remove_tracks_from_playlist(playlist_id, remove_ids)
 
             # Remove duplicates from the list of new ids
+            s_new_ids = str(new_ids)
+            log.debug("new ids BEFORE: " + s_new_ids)
             new_ids = list(set(new_ids))
             s_new_ids = str(new_ids)
-            print("new ids: " + s_new_ids)
+            log.debug("new ids AFTER: " + s_new_ids)
 
             # Add tracks to playlist in batches of 100
             url = f"https://api.deezer.com/playlist/{playlist_id}/tracks"
